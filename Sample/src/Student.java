c57a981fb603e56ce8989976c011faa08ecb82cc
Khaldoon Aqel <kaqel@processandsmith.com>
2020-06-10 16:36:24 +0300

    Added student attributes with getters and setters...

--- Sample/src/Student.java
+++ Sample/src/Student.java
@@ -1,6 +1,30 @@
 public class Student {
+
+    private String name;
+    private int grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
     public static void main(){
         System.out.println("Hello World.");
         System.out.println("Another Message.");
+    }
+
+    public Student() {
+
     }
 }
