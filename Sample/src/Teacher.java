e43b29834ebca045249fe17569417770bbca7fd1
Khaldoon Aqel <kaqel@processandsmith.com>
2020-06-11 09:18:25 +0300

    New Teacher class

---
+++ Sample/src/Teacher.java
@@ -0,0 +1,17 @@
+public class Teacher {
+    private String name;
+    private String address;
+
+    public Teacher(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
