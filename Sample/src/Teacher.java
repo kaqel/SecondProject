decc56e0bc50d257fa0cf6ac76182264bec41fe9
Khaldoon Aqel <kaqel@processandsmith.com>
2020-06-11 13:41:26 +0300

    Added birth date

--- Sample/src/Teacher.java
+++ Sample/src/Teacher.java
@@ -1,8 +1,10 @@
+import java.util.Date;
+
 public class Teacher {
     private String name;
     private String address;
     private String major;
-
+    private Date birthDate;
 
     public Teacher(String name, String address) {
         this.name = name;
