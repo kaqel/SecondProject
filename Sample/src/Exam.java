5ea9a5c23249e495c8d23450527690e67dcdff12
Khaldoon Aqel <kaqel@processandsmith.com>
2020-06-20 10:53:40 +0300

    Added exam class

---
+++ Sample/src/Exam.java
@@ -0,0 +1,16 @@
+public class Exam {
+    private String name;
+    private int duration;
+
+    public Exam() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+}
